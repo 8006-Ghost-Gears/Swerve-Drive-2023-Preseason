e4c009f2c899e70a194832c61afac8e819f1053f
Spooky <61633921+Spoookyyy@users.noreply.github.com>
2023-11-02 16:16:50 -0700

    testRobotJay

--- projecttyp/src/main/java/frc/robot/Constants.java
+++ projecttyp/src/main/java/frc/robot/Constants.java
@@ -12,4 +12,17 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+  // [  Controllers  ]
+  public static int operator = 1;
+  public static int driver = 0;
+
+  // [  Motors  ]
+  public static int leftMotorBack = 2;
+  public static int leftMotorFront = 3;
+  public static int rightMotorBack = 4;
+  public static int rightMotorFront = 5;
+
+
+}
